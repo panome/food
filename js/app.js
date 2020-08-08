1cb04d3e6cf3b4c319499dc9175fa7acd54d5352
Ilya <Omeljan@post.cz>
2020-08-08 06:13:53 +0200

    Complete tabs

--- js/app.js
+++ js/app.js
@@ -1,40 +1,70 @@
 'use strict'
-
-const tabContent = document.querySelectorAll('.tabcontent'),
-		tabheaderParent = document.querySelector('.tabheader__items'),
-		tabheaderItem = tabheaderParent.querySelectorAll('.tabheader__item')
+window.addEventListener('DOMContentLoaded', () => {
 
 
 
-function hideTabContent() {
-	tabContent.forEach(item => {
-		item.classList.add('hide')
-		item.classList.remove('show')
-	})
+	//TABS
+	const tabContent = document.querySelectorAll('.tabcontent'),
+			tabheaderParent = document.querySelector('.tabheader__items'),
+			tabheaderItem = tabheaderParent.querySelectorAll('.tabheader__item')
 
-	tabheaderItem.forEach(item => {
-		item.classList.remove('tabheader__item_active')
-	})
-}
 
-function showTabContent(i = 0) {
-		tabContent[i].classList.add('show')
-		tabContent[i].classList.remove('hide')
-		tabheaderItem[i].classList.add('tabheader__item_active')
-}
+	function hideTabContent() {
+		tabContent.forEach(item => {
+			item.classList.add('hide')
+			item.classList.remove('show')
+		})
 
-hideTabContent()
-showTabContent()
-
-tabheaderParent.addEventListener('click', (e) => {
-	const target = e.target
-
-	if (target && target.classList.contains('tabheader__item')) {
-		tabheaderItem.forEach((item, i) => {
-			if(target == item) {
-				hideTabContent()
-				showTabContent(i)
-			}
+		tabheaderItem.forEach(item => {
+			item.classList.remove('tabheader__item_active')
 		})
 	}
+
+	function showTabContent(i = 0) {
+		tabContent[i].classList.remove('hide')
+		tabContent[i].classList.add('show')
+		tabheaderItem[i].classList.add('tabheader__item_active')
+	}
+
+	hideTabContent()
+	showTabContent()
+
+	tabheaderParent.addEventListener('click', (e) => {
+		const target = e.target
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabheaderItem.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent()
+					showTabContent(i)
+				}
+			})
+		}
+	})
+
+
+	//TIMER
+	//MODAL
+	const modal = document.querySelector('[data-modal]'),
+			modalOpen = document.querySelectorAll('[data-modalOpen]'),
+			modalClose = document.querySelector('[data-modalClose]')
+
+	const showModal = (selectorModal, selectorOpen) => {
+		selectorOpen.forEach(item => {
+			item.addEventListener('click', () => {
+				selectorModal.style.display = 'block'
+				document.body.style.overflow = 'hidden'
+			})
+		})	
+	}
+
+	const hideModal = (selectorModal, selectorClose) => {
+		selectorClose.addEventListener('click', () => {
+			selectorModal.style.display = 'none'
+			document.body.style.overflow = 'scroll'
+		})
+	}
+	showModal(modal, modalOpen)
+	hideModal(modal, modalClose)
+	//SLIDER
 })
