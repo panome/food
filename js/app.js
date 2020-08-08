4b4e441106e192b260b9cde5cdef736f11da0d06
Ilya <Omeljan@post.cz>
2020-08-08 07:28:23 +0200

    Dynamic render cards

--- js/app.js
+++ js/app.js
@@ -75,19 +75,18 @@ window.addEventListener('DOMContentLoaded', () => {
 				seconds = timer.querySelector('#seconds'),
 				minutes = timer.querySelector('#minutes'),
 				hours = timer.querySelector('#hours'),
-				days = timer.querySelector('#days'),
-				timeInterval = setInterval(updateClock, 1000)
-
-		updateClock()
+				days = timer.querySelector('#days')
 
 		function updateClock() {
 			const t = getTimeRemaining(endtime)
-
-			seconds.innerHTML = addZero(t.seconds)
-			minutes.innerHTML = addZero(t.minutes)
-			hours.innerHTML = addZero(t.hours)
-			days.innerHTML = addZero(t.days)
+					seconds.innerHTML = addZero(t.seconds)
+					minutes.innerHTML = addZero(t.minutes)
+					hours.innerHTML = addZero(t.hours)
+					days.innerHTML = addZero(t.days)
 		}
+		updateClock()
+
+		setInterval(updateClock, 1000)
 	}
 	setClock('.timer', deadline)
 
@@ -113,5 +112,64 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 	showModal(modal, modalOpen)
 	hideModal(modal, modalClose)
-	//SLIDER
+	
+	//DYNAMIC CARDS
+	class Cards {
+		constructor (img, alt, title, descr, price, parentSelector) {
+			this.img = img
+			this.alt = alt
+			this.title = title
+			this.descr = descr
+			this.price = price
+			this.parent = document.querySelector(parentSelector)
+		}
+
+		render() {
+			const element = document.createElement('div')
+
+			element.classList.add('menu__item')
+			
+			element.innerHTML = `
+				<img src="${this.img}" alt="${this.alt}">
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+				</div>
+			`
+			this.parent.append(element)
+		}
+	}
+
+	new Cards (
+		'img/tabs/vegy.jpg',
+		'vegy',
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощейи фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой высоким качеством!',
+		'229',
+		'.menu .container'
+	).render()
+
+	new Cards(
+		'img/tabs/elite.jpg',
+		'elite',
+		'Меню "Премиум"',
+		'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		'550',
+		'.menu .container'
+	).render()
+
+	new Cards(
+		'img/tabs/post.jpg',
+		'post',
+		'Меню "Постное"',
+		'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		'430',
+		'.menu .container'
+	).render()
+
+
+
 })
