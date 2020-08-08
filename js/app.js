dd4a5114cab6788aa3b1fb901b27fabf814229b4
Ilya <Omeljan@post.cz>
2020-08-08 17:22:40 +0200

    Add functionaly forms, add server.php

--- js/app.js
+++ js/app.js
@@ -170,6 +170,59 @@ window.addEventListener('DOMContentLoaded', () => {
 		'.menu .container'
 	).render()
 
+	//FORMS
+	const forms = document.querySelectorAll('form')
+
+	const message = {
+		loading: 'Zagruzka',
+		succes: 'Spasibo! Skoro my s vami svyazhemsya',
+		failure: 'Chtoto poshlo ne tak'
+	}
+
+	forms.forEach(item => {
+		postData(item)
+	})
+
+	function postData(form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault()
+
+			const statusMessage = document.createElement('div')
+			statusMessage.classList.add('status')
+			statusMessage.textContent = message.loading
+			form.append(statusMessage)
+
+			const request = new XMLHttpRequest()
+			request.open('POST', 'server.php')
+
+			request.setRequestHeader('Content-type', 'application/json')
+
+			const formData = new FormData(form)
+
+			const object = {}
+
+			formData.forEach((value, key) => {
+				object[key] = value
+			})
+
+			const json = JSON.stringify(object)
+
+			request.send(json)
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					console.log(request.response)
+					statusMessage.textContent = message.succes
+					form.reset()
+					setTimeout(() => {
+						statusMessage.remove()
+					}, 2000)
+				} else {
+					statusMessage.textContent = message.failure
+				}
+			})
+		})
+	}
 
 
 })
